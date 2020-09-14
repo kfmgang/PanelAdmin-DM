41318d9196ab5b3b8c11e432cecfacca6a45fb6a
kfm_gang <kfmroot@protonmail.com>
2020-09-14 15:52:44 +0200

    uptade

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$db = new PDO('sqlite:D:Program Files (x86)/Steam/steamapps/common/Dead Matter Dedicated Server/deadmatter/Saved/sqlite3/SaveData_v01.db');
+$db = new PDO('sqlite:SaveData_v01.db');
 $result = $db->query('SELECT * from GameSession');
 foreach($result as $row){
   print "<td>".$row['BannedUsers']."</td>";
