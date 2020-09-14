d5ff6f23d5e3ec2a320df42b49563a8e4e05acc1
kfm_gang <kfmroot@protonmail.com>
2020-09-14 15:30:59 +0200

    uptade

--- test.php
+++ test.php
@@ -1,10 +1,7 @@
 <?php
-require('vendor/autoload.php');
-include 'INI.class.php'; 
-
-$client = new \Zyberspace\SteamWebApi\Client('C93FFB23FD0B17F012878B8B3FA69379');
-$steamUser = new \Zyberspace\SteamWebApi\Interfaces\ISteamUser($client);
-$response = $steamUser->GetPlayerSummariesV2('76561198179589930');
-
-print_r($response->response->players[0]);
+$db = new PDO('sqlite:D:Program Files (x86)/Steam/steamapps/common/Dead Matter Dedicated Server/deadmatter/Saved/sqlite3/SaveData_v01.db');
+$result = $db->query('SELECT * from GameSession');
+foreach($result as $row){
+  print "<td>".$row['BannedUsers']."</td>";
+}
 ?>
