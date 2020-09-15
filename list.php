c4a3d362b470760ebd3e4bf592b0c7e566476f20
kfmgang <chiovettanoah@gmail.com>
2020-09-15 23:06:36 +0200

    update

--- list.php
+++ list.php
@@ -15,4 +15,22 @@ $variable = [
 ["PVP","Toggles whether or not PVP is enabled. If this is false, no damage can be inflicted by one player on another."],
 ["FallDamageMultiplier","Change how much damage falling does here. 1.0 is full damage, 0 is no damage at all."]
 ];
+$Engine_GameSession = ["MaxPlayers","Maximum player count for the server."];
+
+$Steam = [
+    ["Port","Change the Steam advertised gameserver port. If this is absent it'll just use the server's port."],
+    ["SteamPort","Change the Steam communications port."],
+    ["SteamQueryPort","The port used to query A2S_INFO requests. This is what tells players who's on the server from the server browser."],
+    ["Host","Host to advertise to Steam."],
+];
+
+$DeadMatter_FlockSpawner =["AnimalSpawnMultiplier","How many more animals to spawn than usual."];
+$DeadMatter_GlobalAISpawner =["ZombieSpawnMultiplier","How many more zombies to spawn than usual."];
+$DeadMatter_Agenda =["Timescale","The timescale, relative to real time. The default value of 5.5 indicates that one real-life second is 5.5 seconds ingame."];
+
+$DeadMatter_ZombiePawn = [
+    ["AttackMultiplier","How strongly the zombies do damage. Set to zero to disable."],
+    ["DefenseMultiplier","How much the zombies soak up hits. Set to zero to make them made of paper."],
+    ["SteamQueryPort","The port used to query A2S_INFO requests. This is what tells players who's on the server from the server browser."]
+];
 ?>
